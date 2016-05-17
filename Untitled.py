921f61f1d2fb02bea85efa7960637d25f3fc3599
tsikorksi <sikorski.t@etoncollege.org.uk>
2016-05-17 12:04:47 +0100

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,18 @@
+import time
+
+def main():
+    x = len(word)
+    if x == 9:
+        score = score + 18
+    else:
+        score = score + len(word)
+main()
+
+def time():
+    print("Your time starts...")
+    time.sleep(2)
+    print("Now!")
+    time.sleep(30)
+    print("Times up")
+
+time()
