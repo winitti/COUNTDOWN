9db1f7a39e39ad5e9b96a43dc884c9f35cb77760
tsikorksi <sikorski.t@etoncollege.org.uk>
2016-05-23 09:09:03 +0100

    Update Countdown Menu.py

--- Countdown Menu.py
+++ Countdown Menu.py
@@ -1,3 +1,4 @@
+import time
 def menu():
     Name = input("Please enter your name")
     print("Hello",Name,"welcome to COUNTDOWN!")
@@ -7,6 +8,7 @@ def menu():
         print("Press 1 to play")
         time.sleep(2)
         print("Press 2 to quit")
+        time.sleep(2)
         anyInput = input("Please choose an option ")
         if anyInput == '1':
             game()
